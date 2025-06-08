e681c424944b6815f7c0d1363f70445c78673353
suparnadey-5 <miss.s.dey05@gmail.com>
2025-06-08 10:32:25 +0530

    css removed and portfolio updated

--- suparna-s-portfolio/src/App.tsx
+++ suparna-s-portfolio/src/App.tsx
@@ -1,33 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Hello world!
+       
+        <h1>Suparna"s Portfolio</h1>
+        <p>Welcome to my portfolio website! Here, you can find information about my projects, skills, and experiences. I am a passionate developer with a keen interest in creating innovative solutions. Feel free to explore my work and get in touch if you have any questions or opportunities for collaboration. Thank you for visiting!
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
